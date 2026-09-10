9d87e8bb1f8876eed6614c9f85c0828c92b97018
Braden Fike <bf028624@ohio.edu>
2026-09-10 09:47:08 -0400

    Basic State Transition Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,30 @@
 // put function declarations here:
 int myFunction(int, int);
 
+enum class State{
+  SETUP
+  RUNNING,
+  MENU,
+  ALARM,
+}
+
+State currentState = State::SETUP;
+
+void enterState(State newState) {
+  currentState = newState;
+  switch (currentState) {
+    case State::RUNNING:
+      // Code to execute when entering RUNNING state
+      break;
+    case State::MENU:
+      // Code to execute when entering MENU state
+      break;
+    case State::ALARM:
+      // Code to execute when entering ALARM state
+      break;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
